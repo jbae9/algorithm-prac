63253ae8f1ccf0478e804bcccb42acd01ac795fb
Jin Bae 배진영 <jinbae00@gmail.com>
2023-01-26 20:21:21 +0900

    [Bronze V] Title: A-B, Time: 156 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,8 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+	input:process.stdin
+})
+
+rl.question('', (str) =>
+    str.split(' ').reduce((a,b) => console.log(Number(a) - Number(b))))
