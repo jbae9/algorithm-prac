db97450ca472ba5c6d07b39080262620afebbc45
Jin Bae 배진영 <jinbae00@gmail.com>
2023-02-16 16:13:18 +0900

    [level 0] Title: 문자열 밀기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -2,7 +2,7 @@ function solution(A, B) {
     var pushed = 0
     for (let i=0; i<A.length; i++) {
         if (A === B) return pushed
-        A = A[A.length-1] + A.substring(0,A.length-1)
+        A = A.slice(-1) + A.slice(0,-1)
         pushed++
     }
     return -1
