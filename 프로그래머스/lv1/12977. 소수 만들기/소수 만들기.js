d0a8336e29869f32b5ebf86359623bdf7c784571
Jin Bae 배진영 <jinbae00@gmail.com>
2023-04-25 11:53:57 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,27 @@
+function isPrime (num) {
+    if (num === 1) return true
+    
+    for (let i=2; i<num; i++) {
+        if (num%i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function solution(nums) {
+    let count = 0
+    
+    for (let i=0; i<nums.length-2; i++) {
+        for (let j=i+1; j<nums.length-1; j++) {
+            for (let k=j+1; k<nums.length; k++) {
+                const sum = nums[i]+nums[j]+nums[k]
+                if (isPrime(sum)) {
+                    count++
+                }
+            }
+        }
+    }
+    
+    return count
+}
