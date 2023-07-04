e0bdc07920689190c34f7a0d2d6c1731cbc59ada
Jin Bae 배진영 <jinbae00@gmail.com>
2023-07-04 23:29:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return s.length%2===0 ? s.substring(s.length/2-1, s.length/2+1) : s.substring(Math.max(s.length/2), Math.max(s.length/2)+1)
+    return s.length%2===0 ? s.substring(s.length/2-1, s.length/2+1) : s.substring(Math.floor(s.length/2), Math.floor(s.length/2)+1)
 }
